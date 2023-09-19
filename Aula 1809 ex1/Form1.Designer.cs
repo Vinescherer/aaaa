c6ae9b55e1a350f8f5f2ce270dd0daf58208c90b
Vinescherer <142275233+Vinescherer@users.noreply.github.com>
2023-09-18 21:50:42 -0300

    Add files via upload

---
+++ Aula 1809 ex1/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Aula_1809_ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Resultado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox1Base = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TextBox2Altura = new System.Windows.Forms.TextBox();
+            this.TextBox3Raio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox3VetA = new System.Windows.Forms.ListBox();
+            this.listBox4VetB = new System.Windows.Forms.ListBox();
+            this.listBox5Resu = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Resultado
+            // 
+            this.Resultado.Location = new System.Drawing.Point(45, 12);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(75, 23);
+            this.Resultado.TabIndex = 0;
+            this.Resultado.Text = "button1";
+            this.Resultado.UseVisualStyleBackColor = true;
+            this.Resultado.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Base";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TextBox1Base
+            // 
+            this.TextBox1Base.Location = new System.Drawing.Point(44, 68);
+            this.TextBox1Base.Name = "TextBox1Base";
+            this.TextBox1Base.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1Base.TabIndex = 2;
+            this.TextBox1Base.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Retângulo",
+            "Círculo"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // TextBox2Altura
+            // 
+            this.TextBox2Altura.Location = new System.Drawing.Point(44, 94);
+            this.TextBox2Altura.Name = "TextBox2Altura";
+            this.TextBox2Altura.Size = new System.Drawing.Size(100, 20);
+            this.TextBox2Altura.TabIndex = 4;
+            this.TextBox2Altura.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TextBox3Raio
+            // 
+            this.TextBox3Raio.Location = new System.Drawing.Point(44, 120);
+            this.TextBox3Raio.Name = "TextBox3Raio";
+            this.TextBox3Raio.Size = new System.Drawing.Size(100, 20);
+            this.TextBox3Raio.TabIndex = 5;
+            this.TextBox3Raio.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Altura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Raio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(271, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(194, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(102, 95);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(302, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(102, 95);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "(1)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "(2)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 209);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox3VetA
+            // 
+            this.listBox3VetA.FormattingEnabled = true;
+            this.listBox3VetA.Location = new System.Drawing.Point(18, 238);
+            this.listBox3VetA.Name = "listBox3VetA";
+            this.listBox3VetA.Size = new System.Drawing.Size(120, 95);
+            this.listBox3VetA.TabIndex = 14;
+            // 
+            // listBox4VetB
+            // 
+            this.listBox4VetB.FormattingEnabled = true;
+            this.listBox4VetB.Location = new System.Drawing.Point(144, 238);
+            this.listBox4VetB.Name = "listBox4VetB";
+            this.listBox4VetB.Size = new System.Drawing.Size(120, 95);
+            this.listBox4VetB.TabIndex = 15;
+            // 
+            // listBox5Resu
+            // 
+            this.listBox5Resu.FormattingEnabled = true;
+            this.listBox5Resu.Location = new System.Drawing.Point(83, 339);
+            this.listBox5Resu.Name = "listBox5Resu";
+            this.listBox5Resu.Size = new System.Drawing.Size(120, 95);
+            this.listBox5Resu.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "(3)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(575, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(505, 161);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(102, 95);
+            this.listBox3.TabIndex = 20;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(613, 161);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(102, 95);
+            this.listBox4.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(550, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "(4)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(465, 193);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Pares";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(721, 213);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Multiplos de 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox5Resu);
+            this.Controls.Add(this.listBox4VetB);
+            this.Controls.Add(this.listBox3VetA);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBox3Raio);
+            this.Controls.Add(this.TextBox2Altura);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.TextBox1Base);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBox1Base;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox TextBox2Altura;
+        private System.Windows.Forms.TextBox TextBox3Raio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox3VetA;
+        private System.Windows.Forms.ListBox listBox4VetB;
+        private System.Windows.Forms.ListBox listBox5Resu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Aula 1809 ex1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Aula_1809_ex1
+{
+    public partial class Form1 : Form
+    {
+        double alt, bas, raio, resu;
+        Random randNum = new Random();
+        int menor, maior = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    TextBox1Base.Enabled = true;
+                    TextBox2Altura.Enabled = true;
+
+
+                }
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    bas = double.Parse(TextBox1Base.Text);
+                    alt = double.Parse(TextBox2Altura.Text);
+
+                    resu = bas * alt;
+
+                    MessageBox.Show(" A área do retângulo  é de " + resu);
+
+
+                }
+                if (comboBox1.SelectedIndex == 1)
+                {
+
+
+                    TextBox3Raio.Enabled = true;
+
+                    TextBox1Base.Enabled = false;
+                    TextBox1Base.Clear();
+                    TextBox2Altura.Enabled = false;
+                    TextBox2Altura.Clear();
+
+                }
+
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    raio = double.Parse(TextBox3Raio.Text);
+
+                    resu = raio * raio * 3.14 /2;
+
+                    MessageBox.Show(" A área do círculo  é de " + resu);
+                }
+
+
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message.ToString());
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i;
+            int [] valor1 = new int[10];
+            int menor, maior = 0;
+            Random randNum = new Random();
+
+            try
+            {
+                for (i = 0; i < 10 ; i++)
+                {
+                    valor1[i] = randNum.Next(0, 50);
+                    listBox1.Items.Add(valor1[i]);
+                }
+                for (i = 0; i < 10; i++)
+                {
+                    if (valor1[i] > maior)
+                    {
+                        maior = valor1[i];
+                    }
+                }
+
+                menor = maior; 
+
+                for (i = 0; i < 10; i++)
+                {
+                    if (valor1[i] < menor)
+                    {
+                        menor = valor1[i];
+                    }
+                }
+
+                listBox2.Items.Add(menor);
+                listBox2.Items.Add(maior);
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message.ToString());
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int[] mod1 = new int[15];
+            int[] mod2 = new int[15];
+          
+            try
+            {
+                maior = 0;
+                menor = 0;
+                for (int i = 0; i < 10; i++) 
+                {
+                    mod1[i] = randNum.Next(1, 19);
+                    listBox3VetA.Items.Add(mod1[i]);
+                    mod2[i] = randNum.Next(1, 19);
+                    listBox4VetB.Items.Add(mod2[i]);
+                }
+              
+                for (int i = 0; i < 10; i++)
+                {
+                    if (mod1[i] > maior)
+                    {
+                        maior = mod1[i];
+                    }
+                }
+
+                menor = maior;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (mod2[i] < menor)
+                    {
+                        menor = mod2[i];
+                    }
+                }
+                listBox5Resu.Items.Add(menor * maior);
+                
+                
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message.ToString());
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int[] num = new int[44];
+
+            try
+            {
+                for (int i = 4; i < 8 ;i++) 
+                { 
+                    for (int j = 0;j < 11; j++)
+                    {
+                        num[j] = i * j;
+
+                        if (num[j] %2 == 0)
+                        {
+                            listBox3.Items.Add(num[j]);
+                        }  
+                        if (num[j] %3 == 0)
+                        {
+                            listBox4.Items.Add(num[j]);
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
